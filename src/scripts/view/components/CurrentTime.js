f733be6cd147d349712bc60d0260fd190e95aa67
jokovivanco <jokovivanco@gmail.com>
2023-05-06 20:15:41 +0800

    update address

--- src/scripts/view/components/CurrentTime.js
+++ src/scripts/view/components/CurrentTime.js
@@ -5,8 +5,8 @@ import Lines from './Lines';
 
 const CurrentTime = async () => {
   const date = new Date();
-  // const address = await LocationAPI.getAddress();
-  const addressString = 'address.formatted';
+  const address = await LocationAPI.getAddress();
+  const addressString = address.formatted;
   const month = DateConverter.monthToString(date.getMonth());
   const formattedDateTime = `${date.getDate()} ${month} ${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}`;
 
